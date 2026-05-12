5caef3eeb750ff913119d6aaec35ffcc8eb79235
kyrus-12 <rusky0537@gmail.com>
2026-05-12 20:40:49 +0800

    Change HTML file path to 'public/index.html'

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // Tell the server where your files are
 // Ensure your HTML file is renamed to 'index.html' or update the line below
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
